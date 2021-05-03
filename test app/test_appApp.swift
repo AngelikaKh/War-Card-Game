e76731f8cd9d5c8a81c4c2f9db919a9923a4bbe9
AngelikaKh <angelika1mos@gmail.com>
2021-05-03 17:04:16 +0400

    Initial Commit

---
+++ test app/test_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_appApp.swift
+//  test app
+//
+//  Created by Angelika Khodzhaian on 03.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct test_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
