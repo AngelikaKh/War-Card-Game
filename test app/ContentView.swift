7bc1acfa1369d534d2a4982991deb059256f9756
AngelikaKh <angelika1mos@gmail.com>
2021-05-03 21:50:37 +0400

    addText

--- test app/ContentView.swift
+++ test app/ContentView.swift
@@ -9,8 +9,10 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        Text("Hi, Angelika")
+            .blur(radius: 1.0)
             .padding()
+            .background(Color.blue)
     }
 }
 
